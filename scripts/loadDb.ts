9ce55fc926c58a12a163238d6cfa5594b7147443
anasyakubu <yakubuanas04@gmail.com>
2024-11-15 17:09:46 +0100

    changes and import some packages

---
+++ scripts/loadDb.ts
@@ -0,0 +1,8 @@
+import { DataAPIClient } from "@datastax/astra-db-ts";
+import { PuppeteerWebBaseLoader } from "langchain/document_loaders/web/puppeteer";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { OpenAI } from "openai";
+
+import "dotenv/config";
+
+// Your logic here...
